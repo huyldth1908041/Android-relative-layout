8398fc49e1ed8437b10bff8dff896065122df6e5
huyldth1908041 <huyldth1908041>
2021-08-28 15:57:28 +0700

    relative layout

---
+++ app/src/main/java/com/t1908e/relativelayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.t1908e.relativelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
